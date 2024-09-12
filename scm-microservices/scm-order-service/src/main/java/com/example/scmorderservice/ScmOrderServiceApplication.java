de00eeafa2a27886719621c4ea23eed3b1b57e1f
Erjan93 <cykaa1993@gmail.com>
2024-09-12 14:25:54 +0300

    task002:добавил модуль для заказов

---
+++ scm-microservices/scm-order-service/src/main/java/com/example/scmorderservice/ScmOrderServiceApplication.java
@@ -0,0 +1,14 @@
+package com.example.scmorderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScmOrderServiceApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Hello wolrd!");
+        SpringApplication.run(ScmOrderServiceApplication.class, args);
+    }
+
+}
